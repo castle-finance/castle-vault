691b59b107f7e091e3c2c69896dc0348bb208bd0
Charlie You <charlie.you@hey.com>
2021-12-31 16:02:31 -0600

    Move solend inits to helper mod

--- tests/castle-lending-aggregator.ts
+++ tests/castle-lending-aggregator.ts
@@ -1,12 +1,14 @@
-import { Program } from '@project-serum/anchor';
 import assert from "assert";
-import * as solend from "@dbricks/dbricks-solend";
-import { Buffer } from "buffer";
+import { Program } from '@project-serum/anchor';
 import { AccountLayout, MintLayout, TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
-import { PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY } from "@solana/web3.js";
+import { Keypair, PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY } from "@solana/web3.js";
+
+import { Solend } from './helpers/solend';
 import { CastleLendingAggregator } from "../target/types/castle_lending_aggregator";
 
+// Change to import after https://github.com/project-serum/anchor/issues/1153 is resolved
 const anchor = require("@project-serum/anchor");
+
 
 /// TODO use SDK instead of raw code
 describe("castle-vault", () => {
@@ -25,53 +27,30 @@ describe("castle-vault", () => {
   const vaultStateAccount = anchor.web3.Keypair.generate();
   const payer = anchor.web3.Keypair.generate();
 
-  // TODO add private key to fixtures
-  const lendingProgram = new PublicKey("BwTGCAdzPncEFqP5JBAeCLRWKE8MDVvbGDVMD7XX2fvu");
+  let solendMarket: Keypair;
+  let solendMarketAuthority: PublicKey;
 
-  const quoteCurrency = (s: string) => {
-    const buf = Buffer.alloc(32);
-    const strBuf = Buffer.from(s);
-    strBuf.copy(buf, 0);
-    return buf;
-  };
+  const solendProgramId = new PublicKey("BwTGCAdzPncEFqP5JBAeCLRWKE8MDVvbGDVMD7XX2fvu") 
+  const solendProgram = new Solend(provider, solendProgramId);
 
-  const lendingMarket = anchor.web3.Keypair.generate();
-  const collateralMint = anchor.web3.Keypair.generate();
-  const reserve = anchor.web3.Keypair.generate();
-  const liquiditySupply = anchor.web3.Keypair.generate();
+  const solendCollateralMint = anchor.web3.Keypair.generate();
+  const solendReserve = anchor.web3.Keypair.generate();
+  const solendLiquiditySupply = anchor.web3.Keypair.generate();
 
   const pythProduct = new PublicKey("3Mnn2fX6rQyUsyELYms1sBJyChWofzSNRoqYzvgMVz5E");
   const pythPrice = new PublicKey("J83w4HKfqxwcq3BEMMkPFSppX3gqekLyLJBexebFVkix");
   const switchboardFeed = new PublicKey("GvDMxPzN1sCj7L26YDK2HnMRXEQmQ2aemov8YBtPS7vR");
 
-  let lendingMarketAuthority: PublicKey;
-
   before(async () => {
     const sig  = await provider.connection.requestAirdrop(payer.publicKey, 1000000000);
     await provider.connection.confirmTransaction(sig, "singleGossip");
 
-    const pythProgramId = new PublicKey("gSbePebfvPy7tRqimPoVecS2UsBvYv46ynrzWocc92s");
-    const switchboardProgramId = new PublicKey("2TfB33aLaneQb5TNVwyDz3jSZXS6jdW2ARw1Dgf84XCG");
-    const balanceNeeded = await provider.connection.getMinimumBalanceForRentExemption(solend.LENDING_MARKET_SIZE);
-    const initTx = new anchor.web3.Transaction().add(
-      SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: lendingMarket.publicKey,
-        lamports: balanceNeeded,
-        space: solend.LENDING_MARKET_SIZE,
-        programId: lendingProgram,
-      })
-    ).add(
-      solend.initLendingMarketInstruction(
-        owner.publicKey,
-        quoteCurrency("USD"),
-        lendingMarket.publicKey,
-        pythProgramId,
-        switchboardProgramId,
-        lendingProgram
-      )
+    solendMarket = await solendProgram.initLendingMarket(
+      owner.publicKey,
+      payer,
+      new PublicKey("gSbePebfvPy7tRqimPoVecS2UsBvYv46ynrzWocc92s"),
+      new PublicKey("2TfB33aLaneQb5TNVwyDz3jSZXS6jdW2ARw1Dgf84XCG"),
     );
-    await provider.send(initTx, [payer, lendingMarket]);
 
     reserveTokenMint = await Token.createMint(
       provider.connection,
@@ -81,129 +60,26 @@ describe("castle-vault", () => {
       2,
       TOKEN_PROGRAM_ID
     );
-    const ownerReserveTokenAccount = await reserveTokenMint.createAccount(owner.publicKey);
-    const liquidityAmount = 10;
-    await reserveTokenMint.mintTo(ownerReserveTokenAccount, owner, [], liquidityAmount);
 
-    const collateralSupply = anchor.web3.Keypair.generate();
-    const liquidityFeeReceiver = anchor.web3.Keypair.generate();
-    const userCollateral = anchor.web3.Keypair.generate();
-    const userTransferAuthority = anchor.web3.Keypair.generate();
-
-    const reserveBalance = await provider.connection.getMinimumBalanceForRentExemption(solend.RESERVE_SIZE);
-    const mintBalance = await provider.connection.getMinimumBalanceForRentExemption(MintLayout.span);
-    const accountBalance = await provider.connection.getMinimumBalanceForRentExemption(AccountLayout.span);
-
-    const tx1 = new anchor.web3.Transaction().add(
-      SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: reserve.publicKey,
-        lamports: reserveBalance,
-        space: solend.RESERVE_SIZE,
-        programId: lendingProgram,
-      })
-    ).add(
-      SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: collateralMint.publicKey,
-        lamports: mintBalance,
-        space: MintLayout.span,
-        programId: TOKEN_PROGRAM_ID,
-      })
-    ).add(
-      SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: collateralSupply.publicKey,
-        lamports: accountBalance,
-        space: AccountLayout.span,
-        programId: TOKEN_PROGRAM_ID,
-      })
-    ).add(
-      SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: userCollateral.publicKey,
-        lamports: accountBalance,
-        space: AccountLayout.span,
-        programId: TOKEN_PROGRAM_ID,
-      })
-    );
-    const tx2 = new anchor.web3.Transaction().add(
-      SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: liquiditySupply.publicKey,
-        lamports: accountBalance,
-        space: AccountLayout.span,
-        programId: TOKEN_PROGRAM_ID,
-      })
-    ).add(
-      SystemProgram.createAccount({
-        fromPubkey: payer.publicKey,
-        newAccountPubkey: liquidityFeeReceiver.publicKey,
-        lamports: accountBalance,
-        space: AccountLayout.span,
-        programId: TOKEN_PROGRAM_ID,
-      })
+    [solendMarketAuthority, ] = await PublicKey.findProgramAddress(
+      [solendMarket.publicKey.toBuffer()],
+      solendProgramId,
     );
 
-    [lendingMarketAuthority, ] = await PublicKey.findProgramAddress(
-      [lendingMarket.publicKey.toBuffer()],
-      lendingProgram,
+    await solendProgram.addReserve(
+      owner,
+      payer,
+      reserveTokenMint,
+      solendReserve,
+      solendCollateralMint,
+      solendLiquiditySupply,
+      pythProduct,
+      pythPrice,
+      switchboardFeed,
+      solendMarket.publicKey,
+      solendMarketAuthority,
     );
-    const reserveConfig = {
-      optimalUtilizationRate: 80,
-      loanToValueRatio: 50,
-      liquidationBonus: 5,
-      liquidationThreshold: 55,
-      minBorrowRate: 0,
-      optimalBorrowRate: 4,
-      maxBorrowRate: 30,
-      fees: {
-          /// 0.00001% (Aave borrow fee)
-          borrowFeeWad: 100_000_000_000n,
-          /// 0.3% (Aave flash loan fee)
-          flashLoanFeeWad: 3_000_000_000_000_000n,
-          hostFeePercentage: 20,
-      },
-      depositLimit: 100_000_000n,
-      borrowLimit: 100_000_000n,
-      feeReceiver: liquidityFeeReceiver.publicKey,
-    };
-    const tx3 = new anchor.web3.Transaction().add(
-      Token.createApproveInstruction(
-        TOKEN_PROGRAM_ID,
-        ownerReserveTokenAccount,
-        userTransferAuthority.publicKey,
-        owner.publicKey,
-        [],
-        liquidityAmount,
-      )
-    ).add(
-      solend.initReserveInstruction(
-        liquidityAmount,
-        reserveConfig,
-        ownerReserveTokenAccount,
-        userCollateral.publicKey,
-        reserve.publicKey,
-        reserveTokenMint.publicKey,
-        liquiditySupply.publicKey,
-        liquidityFeeReceiver.publicKey,
-        pythProduct,
-        pythPrice,
-        collateralMint.publicKey,
-        collateralSupply.publicKey,
-        lendingMarket.publicKey,
-        lendingMarketAuthority,
-        owner.publicKey,
-        userTransferAuthority.publicKey,
-        switchboardFeed,
-        lendingProgram,
-      )
-    );
-    await provider.sendAll([
-        {tx: tx1, signers: [payer, reserve, collateralMint, collateralSupply, userCollateral]},
-        {tx: tx2, signers: [payer, liquiditySupply, liquidityFeeReceiver]},
-        {tx: tx3, signers: [owner, userTransferAuthority]},
-    ]);
+
   });
 
   it("Creates vault", async () => {
@@ -250,7 +126,7 @@ describe("castle-vault", () => {
     assert.equal(lpTokenMintInfo.supply.toNumber(), 1000000);
   });
 
-  let userLpTokenAccount;
+  let userLpTokenAccount: PublicKey;
   const depositAmount = 1000;
   it("Deposits to vault reserves", async () => {
     // Create depositor token account
@@ -299,8 +175,8 @@ describe("castle-vault", () => {
   });
 
   it("Forwards deposits to lending program", async () => {
-    const collateralMintToken = new Token(provider.connection, collateralMint.publicKey, TOKEN_PROGRAM_ID, payer);
-    const vaultLpTokenAccount = await collateralMintToken.createAccount(vaultAuthority);
+    const solendCollateralMintToken = new Token(provider.connection, solendCollateralMint.publicKey, TOKEN_PROGRAM_ID, payer);
+    const vaultLpTokenAccount = await solendCollateralMintToken.createAccount(vaultAuthority);
 
     await program.rpc.rebalance(
       {
@@ -309,12 +185,12 @@ describe("castle-vault", () => {
           vaultAuthority: vaultAuthority,
           vaultReserveToken: vaultReserveTokenAccount,
           vaultLpToken: vaultLpTokenAccount,
-          solendProgram: lendingProgram,
-          solendMarketAuthority: lendingMarketAuthority,
-          solendMarket: lendingMarket.publicKey,
-          solendReserveStateAccount: reserve.publicKey,
-          solendLpMintAccount: collateralMint.publicKey,
-          solendDepositTokenAccount: liquiditySupply.publicKey,
+          solendProgram: solendProgramId,
+          solendMarketAuthority: solendMarketAuthority,
+          solendMarket: solendMarket.publicKey,
+          solendReserveStateAccount: solendReserve.publicKey,
+          solendLpMintAccount: solendCollateralMint.publicKey,
+          solendDepositTokenAccount: solendLiquiditySupply.publicKey,
           solendPyth: pythPrice,
           solendSwitchboard: switchboardFeed,
           clock: SYSVAR_CLOCK_PUBKEY,
@@ -325,10 +201,10 @@ describe("castle-vault", () => {
     const vaultReserveTokenAccountInfo = await reserveTokenMint.getAccountInfo(vaultReserveTokenAccount);
     assert.equal(vaultReserveTokenAccountInfo.amount.toNumber(), 0);
 
-    const vaultLpTokenAccountInfo = await collateralMintToken.getAccountInfo(vaultLpTokenAccount);
+    const vaultLpTokenAccountInfo = await solendCollateralMintToken.getAccountInfo(vaultLpTokenAccount);
     assert.notEqual(vaultLpTokenAccountInfo.amount.toNumber(), 0);
 
-    const liquiditySupplyAccountInfo = await reserveTokenMint.getAccountInfo(liquiditySupply.publicKey);
+    const liquiditySupplyAccountInfo = await reserveTokenMint.getAccountInfo(solendLiquiditySupply.publicKey);
     assert.equal(liquiditySupplyAccountInfo.amount.toNumber(), 2010);
   });
 
--- tests/helpers/solend.ts
+++ tests/helpers/solend.ts
@@ -1,11 +1,183 @@
-import { Provider } from "@project-serum/anchor";
+import * as solend from "@dbricks/dbricks-solend";
+import * as anchor from "@project-serum/anchor";
+import { AccountLayout, MintLayout, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { Keypair, PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY } from "@solana/web3.js";
 
-const PROGRAM_ID = "BwTGCAdzPncEFqP5JBAeCLRWKE8MDVvbGDVMD7XX2fvu";
 
 export class Solend {
-    provider: Provider;
+    provider: anchor.Provider;
+    lendingProgram: PublicKey;
 
-    constructor(provider: Provider) {
+    constructor(provider: anchor.Provider, programId: PublicKey) {
         this.provider = provider;
+        this.lendingProgram = programId;
+    }
+
+    async initLendingMarket(owner: PublicKey, payer: Keypair, pythProgramId: PublicKey, switchboardProgramId: PublicKey) {
+        const lendingMarketAccount = anchor.web3.Keypair.generate();
+        const balanceNeeded = await this.provider.connection.getMinimumBalanceForRentExemption(solend.LENDING_MARKET_SIZE);
+
+        const initTx = new anchor.web3.Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: lendingMarketAccount.publicKey,
+                lamports: balanceNeeded,
+                space: solend.LENDING_MARKET_SIZE,
+                programId: this.lendingProgram,
+            })
+        ).add(
+            solend.initLendingMarketInstruction(
+                owner,
+                quoteCurrency("USD"),
+                lendingMarketAccount.publicKey,
+                pythProgramId,
+                switchboardProgramId,
+                this.lendingProgram
+            )
+        );
+        await this.provider.send(initTx, [payer, lendingMarketAccount]);
+        return lendingMarketAccount;
+    }
+
+    async addReserve(
+        owner: Keypair, 
+        payer: Keypair, 
+        reserveTokenMint: Token, 
+        reserve: Keypair, 
+        collateralMint: Keypair,
+        liquiditySupply: Keypair,
+        pythProduct: PublicKey,
+        pythPrice: PublicKey,
+        switchboardFeed: PublicKey,
+        lendingMarket: PublicKey,
+        lendingMarketAuthority: PublicKey,
+    ) {
+        const ownerReserveTokenAccount = await reserveTokenMint.createAccount(owner.publicKey);
+        const liquidityAmount = 10;
+        await reserveTokenMint.mintTo(ownerReserveTokenAccount, owner, [], liquidityAmount);
+
+        const collateralSupply = anchor.web3.Keypair.generate();
+        const liquidityFeeReceiver = anchor.web3.Keypair.generate();
+        const userCollateral = anchor.web3.Keypair.generate();
+        const userTransferAuthority = anchor.web3.Keypair.generate();
+
+        const reserveBalance = await this.provider.connection.getMinimumBalanceForRentExemption(solend.RESERVE_SIZE);
+        const mintBalance = await this.provider.connection.getMinimumBalanceForRentExemption(MintLayout.span);
+        const accountBalance = await this.provider.connection.getMinimumBalanceForRentExemption(AccountLayout.span);
+
+        const tx1 = new anchor.web3.Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: reserve.publicKey,
+                lamports: reserveBalance,
+                space: solend.RESERVE_SIZE,
+                programId: this.lendingProgram,
+            })
+        ).add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: collateralMint.publicKey,
+                lamports: mintBalance,
+                space: MintLayout.span,
+                programId: TOKEN_PROGRAM_ID,
+            })
+        ).add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: collateralSupply.publicKey,
+                lamports: accountBalance,
+                space: AccountLayout.span,
+                programId: TOKEN_PROGRAM_ID,
+            })
+        ).add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: userCollateral.publicKey,
+                lamports: accountBalance,
+                space: AccountLayout.span,
+                programId: TOKEN_PROGRAM_ID,
+            })
+        );
+        const tx2 = new anchor.web3.Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: liquiditySupply.publicKey,
+                lamports: accountBalance,
+                space: AccountLayout.span,
+                programId: TOKEN_PROGRAM_ID,
+            })
+        ).add(
+            SystemProgram.createAccount({
+                fromPubkey: payer.publicKey,
+                newAccountPubkey: liquidityFeeReceiver.publicKey,
+                lamports: accountBalance,
+                space: AccountLayout.span,
+                programId: TOKEN_PROGRAM_ID,
+            })
+        );
+
+        const reserveConfig = {
+            optimalUtilizationRate: 80,
+            loanToValueRatio: 50,
+            liquidationBonus: 5,
+            liquidationThreshold: 55,
+            minBorrowRate: 0,
+            optimalBorrowRate: 4,
+            maxBorrowRate: 30,
+            fees: {
+                /// 0.00001% (Aave borrow fee)
+                borrowFeeWad: 100_000_000_000n,
+                /// 0.3% (Aave flash loan fee)
+                flashLoanFeeWad: 3_000_000_000_000_000n,
+                hostFeePercentage: 20,
+            },
+            depositLimit: 100_000_000n,
+            borrowLimit: 100_000_000n,
+            feeReceiver: liquidityFeeReceiver.publicKey,
+        };
+
+        const tx3 = new anchor.web3.Transaction().add(
+            Token.createApproveInstruction(
+                TOKEN_PROGRAM_ID,
+                ownerReserveTokenAccount,
+                userTransferAuthority.publicKey,
+                owner.publicKey,
+                [],
+                liquidityAmount,
+            )
+        ).add(
+            solend.initReserveInstruction(
+                liquidityAmount,
+                reserveConfig,
+                ownerReserveTokenAccount,
+                userCollateral.publicKey,
+                reserve.publicKey,
+                reserveTokenMint.publicKey,
+                liquiditySupply.publicKey,
+                liquidityFeeReceiver.publicKey,
+                pythProduct,
+                pythPrice,
+                collateralMint.publicKey,
+                collateralSupply.publicKey,
+                lendingMarket,
+                lendingMarketAuthority,
+                owner.publicKey,
+                userTransferAuthority.publicKey,
+                switchboardFeed,
+                this.lendingProgram,
+            )
+        );
+        await this.provider.sendAll([
+            {tx: tx1, signers: [payer, reserve, collateralMint, collateralSupply, userCollateral]},
+            {tx: tx2, signers: [payer, liquiditySupply, liquidityFeeReceiver]},
+            {tx: tx3, signers: [owner, userTransferAuthority]},
+        ]);
     }
 }
+
+const quoteCurrency = (s: string) => {
+    const buf = Buffer.alloc(32);
+    const strBuf = Buffer.from(s);
+    strBuf.copy(buf, 0);
+    return buf;
+};
