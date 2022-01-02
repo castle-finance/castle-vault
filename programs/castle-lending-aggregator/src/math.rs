85cf55a7c01875c5596a36eab11b9c64a0079ff4
Charlie You <charlie.you@hey.com>
2022-01-02 12:19:59 -0600

    Fix math typo

--- programs/castle-lending-aggregator/src/math.rs
+++ programs/castle-lending-aggregator/src/math.rs
@@ -5,12 +5,12 @@ use std::convert::TryFrom;
 pub fn calc_deposit_to_vault(
     reserve_token_amount: u64, lp_token_supply: u64, reserve_tokens_in_vault: u64
 ) -> Option<u64> {
-    let reserve_token_amount = PreciseNumber::new(reserve_token_amount as u128)?;
+    let lp_token_amount = PreciseNumber::new(reserve_token_amount as u128)?;
     let lp_token_supply = PreciseNumber::new(lp_token_supply as u128)?;
     let reserve_tokens_in_vault = PreciseNumber::new(reserve_tokens_in_vault as u128)?;
 
     let lp_tokens_to_mint = lp_token_supply.checked_mul(
-        &reserve_token_amount.checked_div(&reserve_tokens_in_vault)?
+        &lp_token_amount.checked_div(&reserve_tokens_in_vault)?
     )?.to_imprecise()?;
 
     u64::try_from(lp_tokens_to_mint).ok()
@@ -28,4 +28,11 @@ pub fn calc_withdraw_from_vault(
     )?.to_imprecise()?;
 
     u64::try_from(reserve_tokens_to_transfer).ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    // TODO add tests
 }
